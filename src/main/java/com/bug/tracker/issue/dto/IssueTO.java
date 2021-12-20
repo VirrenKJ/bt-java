0a3266ecb7bb6d4e14ce1c69163a147e2e91e977
VirrenKJ <virendra.jarwal@gmail.com>
2021-12-20 11:01:29 +0530

    IssueBO

--- src/main/java/com/bug/tracker/issue/dto/IssueTO.java
+++ src/main/java/com/bug/tracker/issue/dto/IssueTO.java
@@ -1,4 +1,26 @@
 package com.bug.tracker.issue.dto;
 
-public class IssueTO {
+import com.bug.tracker.common.object.AuditTO;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IssueTO extends AuditTO {
+
+  private Integer id;
+  private Integer projectId;
+  private Integer categoryId;
+  private Integer reproducibilityId;
+  private Integer severityId;
+  private Integer priorityId;
+  private Integer profileId;
+  private Integer assignedId;
+  private String summary;
+  private String description;
+  private String stepsToReproduce;
+  private String addInfo;
+  private Integer documentId;
+  private String viewStatus;
+  private boolean deleteFlag;
 }
--- src/main/java/com/bug/tracker/issue/entity/IssueBO.java
+++ src/main/java/com/bug/tracker/issue/entity/IssueBO.java
@@ -1,4 +1,64 @@
 package com.bug.tracker.issue.entity;
 
-public class IssueBO {
+import com.bug.tracker.common.object.Audit;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "project")
+public class IssueBO extends Audit {
+
+  private static final long serialVersionUID = -4125598754816033077L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "project_id")
+  private Integer projectId;
+
+  @Column(name = "category_id")
+  private Integer categoryId;
+
+  @Column(name = "reproducibility_id")
+  private Integer reproducibilityId;
+
+  @Column(name = "severity_id")
+  private Integer severityId;
+
+  @Column(name = "priority_id")
+  private Integer priorityId;
+
+  @Column(name = "profile_id")
+  private Integer profileId;
+
+  @Column(name = "assigned_id")
+  private Integer assignedId;
+
+  @Column(name = "summary")
+  private String summary;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "steps_to_reproduce")
+  private String stepsToReproduce;
+
+  @Column(name = "add_info")
+  private String addInfo;
+
+  @Column(name = "document_id")
+  private Integer documentId;
+
+  @Column(name = "view_status")
+  private String viewStatus;
+
+  @Column(name = "delete_flag")
+  private boolean deleteFlag;
+
 }
