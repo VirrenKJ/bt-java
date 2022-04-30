bbf486faaca71956dc652746f66a3c2a7cd4e876
VirrenKJ <virendra.jarwal@gmail.com>
2022-05-01 00:06:30 +0530

    build

---
+++ src/test/java/com/bug/tracker/config/BtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bug.tracker.bt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
