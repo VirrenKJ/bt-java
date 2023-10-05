89963dad3093d758746a415703ccdb9d15957222
Virendra Jarwal <virendra.jarwal@gmail.com>
2023-10-05 20:15:51 +0530

    refinement

--- src/main/java/com/bug/tracker/config/JwtUtil.java
+++ src/main/java/com/bug/tracker/config/JwtUtil.java
@@ -37,12 +37,8 @@ public class JwtUtil {
   }
 
   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-    final Claims claims = extractAllClaims(token);
+    final Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     return claimsResolver.apply(claims);
-  }
-
-  private Claims extractAllClaims(String token) {
-    return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
   }
 
   private Boolean isTokenExpired(String token) {
@@ -51,14 +47,13 @@ public class JwtUtil {
 
   public String generateToken(UserDetails userDetails) {
     Map<String, Object> claims = new HashMap<>();
-    return createToken(claims, userDetails.getUsername());
-  }
-
-  private String createToken(Map<String, Object> claims, String subject) {
-
-    return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+    return Jwts.builder()
+            .setClaims(claims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 30)))
-            .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+            .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+            .compact();
   }
 
   public Boolean validateToken(String token, UserDetails userDetails) {
